223a8b85cc0bdeb7f062624c50225c9af5e7068d
AlexKarpinski <raptoooor@mail.ru>
2021-03-26 10:53:24 +0300

    update helper

--- helper/helper.js
+++ helper/helper.js
@@ -1,27 +1,10 @@
-let Chance = require('chance');
-let cuisinesData = require("../data/cuisinesData");
-
 class Helper {
 
-    getRandomCuisines(count) {
-        let listOfCuisines = [];
-        for (let i = 0; i < count; i++) {
-            let CUISINE = Object.values(cuisinesData.cuisine)[Chance().integer({min: 0, max: 9})];
-            if (!listOfCuisines.includes(CUISINE)) {
-                listOfCuisines.push(CUISINE);
-            }
-        }
-        return listOfCuisines
-    };
-
     getCountOfRestaurantsFromCuisines(cuisines) {
-        let resultCount = 0;
-        cuisines.forEach(cuisine => {
-            let countOfCuisines = cuisine.restaurants.length;
-            resultCount = resultCount + countOfCuisines;
-        });
-        return resultCount;
+        let initialValue = 0;
+        return cuisines.reduce((sum, cuisine) => sum + cuisine.restaurants.length, initialValue);
     };
 
 }
+
 module.exports = new Helper();
