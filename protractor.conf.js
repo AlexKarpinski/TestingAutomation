5ef92549bc8c95d8dea4521503a5d9eb902f0287
AlexKarpinski <raptoooor@mail.ru>
2021-03-26 11:50:16 +0300

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,7 +9,7 @@ exports.config = {
     capabilities: {
         browserName: "chrome",
         shardTestFiles: true,
-        maxInstances: 4
+        maxInstances: 2
     },
 
     baseUrl: 'https://lit-basin-41473.herokuapp.com/#/',
@@ -22,11 +22,14 @@ exports.config = {
         filterByCuisines: ['tests/homePage.filterByCuisines.spec.js'],
         filterByRating: ['tests/homePage.filterByRating.spec.js'],
         filterByPrice: ['tests/homePage.filterByPrice.spec.js'],
-        filterByCombinedParameters: ['tests/homePage.filterByCombinedParameters.spec.js']
+        filterByCombinedParameters: ['tests/homePage.filterByCombinedParameters.spec.js'],
+        allSuites: ['tests/*.js']
     },
     onPrepare: function () {
-        global.EC = protractor.ExpectedConditions
+        global.EC = protractor.ExpectedConditions;
         let log4js = require('log4js');
+        let Chance = require('chance');
+        global.Chance = require('chance').Chance();
         global.logger = require('log4js').getLogger();
         global.logger.level = 'info';
     }
