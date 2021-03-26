d48895862025bf5e29f0eb668e75da6cb7704987
AlexKarpinski <raptoooor@mail.ru>
2021-03-26 17:40:44 +0300

    Update data

--- data/priceData.js
+++ data/priceData.js
@@ -2,11 +2,11 @@
 
 module.exports = {
     totalNumberOfRestaurant: 39,
-    prices: {
-        "ONE_PRICE_VALUE": {price: 1, numberOfRestaurantWithPrice: 4},
-        "TWO_PRICE_VALUE": {price: 2, numberOfRestaurantWithPrice: 7},
-        "THREE_PRICE_VALUE": {price: 3, numberOfRestaurantWithPrice: 13},
-        "FOUR_PRICE_VALUE": {price: 4, numberOfRestaurantWithPrice: 11},
-        "FIVE_PRICE_VALUE": {price: 5, numberOfRestaurantWithPrice: 4}
-    }
+    prices: [
+        {price: 1, numberOfRestaurantWithPrice: 4},
+        {price: 2, numberOfRestaurantWithPrice: 7},
+        {price: 3, numberOfRestaurantWithPrice: 13},
+        {price: 4, numberOfRestaurantWithPrice: 11},
+        {price: 5, numberOfRestaurantWithPrice: 4}
+    ]
 };
--- data/ratingData.js
+++ data/ratingData.js
@@ -2,11 +2,11 @@
 
 module.exports = {
     totalNumberOfRestaurant: 39,
-    ratings: {
-        "RATING_ONE_STAR": {rating: 1, numberOfRestaurantWithRating: 4},
-        "RATING_TWO_STARS": {rating: 2, numberOfRestaurantWithRating: 10},
-        "RATING_THREE_STARS": {rating: 3, numberOfRestaurantWithRating: 7},
-        "RATING_FOUR_STARS": {rating: 4, numberOfRestaurantWithRating: 10},
-        "RATING_FIVE_STARS": {rating: 5, numberOfRestaurantWithRating: 8}
-    }
+    ratings: [
+        {rating: 1, numberOfRestaurantWithRating: 4},
+        {rating: 2, numberOfRestaurantWithRating: 10},
+        {rating: 3, numberOfRestaurantWithRating: 7},
+        {rating: 4, numberOfRestaurantWithRating: 10},
+        {rating: 5, numberOfRestaurantWithRating: 8}
+    ]
 };
