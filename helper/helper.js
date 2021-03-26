67511100b9b7a3771be50b7af9cd0a2365c55f85
AlexKarpinski <raptoooor@mail.ru>
2021-03-26 21:13:37 +0300

    Update helper.js

--- helper/helper.js
+++ helper/helper.js
@@ -1,9 +1,36 @@
 class Helper {
 
-    getCountOfRestaurantsFromCuisines(cuisines) {
-        let initialValue = 0;
-        return cuisines.reduce((sum, cuisine) => sum + cuisine.restaurants.length, initialValue);
+    getRestaurantsWithCuisine(restaurants, cuisine) {
+        return restaurants.filter(restaurant => restaurant.cuisine === cuisine);
     };
+
+    getCuisinesFromRestaurants(restaurants) {
+       let cuisines = restaurants.map(restaurants => restaurants.cuisine);
+       return cuisines.filter((item, index) => cuisines.indexOf(item)===index);
+    }
+
+    getNumberOfRestaurantsWithCuisines(restaurants, cuisines) {
+        let initialValue = 0;
+        return cuisines.reduce((sum, cuisine) => sum + this.getRestaurantsWithCuisine(restaurants, cuisine).length, initialValue);
+    }
+
+    getPricesFromRestaurants(restaurants) {
+        let prices = restaurants.map(restaurants => restaurants.price);
+        return prices.filter((item, index) => prices.indexOf(item)===index);
+    }
+
+    getRestaurantsWithPrice(restaurants, price) {
+        return restaurants.filter(restaurant => restaurant.price === price);
+    }
+
+    getRatingsFromRestaurants(restaurants) {
+        let ratings = restaurants.map(restaurants => restaurants.rating);
+        return ratings.filter((item, index) => ratings.indexOf(item)===index);
+    }
+
+    getRestaurantsWithRating(restaurants, rating) {
+        return restaurants.filter(restaurant => restaurant.price === rating);
+    }
 
 }
 
